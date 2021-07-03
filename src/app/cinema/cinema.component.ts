ed85df8dadd50cad7d58330ef4f0cc73af15e026
DEVGEEKTM <ismailostmar@gmail.com>
2021-07-03 16:44:40 +0100

    New Component.

--- src/app/cinema/cinema.component.ts
+++ src/app/cinema/cinema.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {CinemaService} from "../services/cinema.service";
 
 @Component({
   selector: 'app-cinema',
@@ -9,12 +10,17 @@ import {HttpClient} from "@angular/common/http";
 export class CinemaComponent implements OnInit {
 
   public villes:any;
+  public cinemas:any;
+  public currentVille:any;
+  public currentCinema:any;
+  public salles:any;
+  public currentProjection: any;
 
-  constructor(private http:HttpClient) { }
+  constructor(private cinemaService:CinemaService) { }
 
   ngOnInit(): void {
       // envoyer une requete httpClientModule
-    this.http.get("http://localhost:8082/villes")
+    this.cinemaService.getVilles()
       .subscribe(data=>{
         this.villes=data;
       },error =>{
@@ -22,4 +28,35 @@ export class CinemaComponent implements OnInit {
       } )
   }
 
+  onGetCinemas(v: any) {
+    this.currentVille=v;
+    this.cinemaService.getCinemas(v)
+      .subscribe(data=>{
+        this.cinemas=data;
+      },error =>{
+        console.log(error);
+      } )
+  }
+
+  onGetSalles(c: any) {
+    this.currentCinema=c;
+    this.cinemaService.getSalles(c)
+      .subscribe(data=> {
+        this.salles=data;
+        this.salles._embedded.salles.forEach((s: { projections: Object; })=>{
+          this.cinemaService.getProjection(s)
+            .subscribe(data=> {
+              s.projections=data;
+            },err=>{
+              console.log(err);
+            })
+        })
+        },err=>{
+        console.log(err);
+      })
+  }
+
+  onGetTicketsPlaces(p: any) {
+
+  }
 }
